a0b24f9a5e9cfad8a6b9e9b96d5e107407182546
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2021-05-30 15:22:43 +0900

    Added ClassroomServiceHelper file.

---
+++ app/src/main/java/app/mobilecontests/onlinegcapplication/ClassroomServiceHelper.java
@@ -0,0 +1,48 @@
+package app.mobilecontests.onlinegcapplication;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.api.services.classroom.Classroom;
+import com.google.api.services.classroom.model.Course;
+import com.google.api.services.classroom.model.CourseWork;
+import com.google.api.services.classroom.model.ListCourseWorkResponse;
+import com.google.api.services.classroom.model.ListCoursesResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ClassroomServiceHelper {
+
+    private final Executor executor = Executors.newFixedThreadPool(1024);
+    private final Classroom classroomService;
+    String pageToken = null;
+
+    public ClassroomServiceHelper(Classroom ClassroomService) {
+        classroomService = ClassroomService;
+    }
+
+    public Task<List<Course>> listCourses() {
+        return Tasks.call(executor, () -> {
+            List<Course> courses = new ArrayList<>();
+            do {
+                ListCoursesResponse response = classroomService.courses().list()
+                        .setPageSize(100)
+                        .setPageToken(pageToken)
+                        .execute();
+                courses.addAll(response.getCourses());
+                pageToken = response.getNextPageToken();
+            } while (pageToken != null);
+            return courses;
+        });
+    }
+
+    public Task<List<CourseWork>> listCourseWorks(final String courseId) {
+        return Tasks.call(executor, () -> {
+            ListCourseWorkResponse response = classroomService.courses().
+                    courseWork().list(courseId).execute();
+            return response.getCourseWork();
+        });
+    }
+}

bd8fab4a95d176b0db1ea6f5c8ceebce212010fe
lnJoy <50934835+lnJoy@users.noreply.github.com>
2021-05-31 01:01:31 +0900

    Add school image animation

--- app/src/main/java/app/mobilecontests/onlinegcapplication/MainActivity.java
+++ app/src/main/java/app/mobilecontests/onlinegcapplication/MainActivity.java
@@ -1,41 +1,36 @@
 package app.mobilecontests.onlinegcapplication;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.Scope;
-import com.google.android.gms.tasks.Task;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.classroom.Classroom;
-import com.google.api.services.classroom.ClassroomScopes;
-import com.google.api.services.classroom.model.Course;
-import com.google.api.services.classroom.model.CourseWork;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
 
-    private Button startBtn;
+    Animation topAnim;
+    Animation bottomAnim;
+
+    ImageView school;
+    Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.school_animation);
+
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.splash_school_animation);
+        bottomAnim.setRepeatCount(Animation.INFINITE);
+        bottomAnim.setRepeatMode(Animation.RESTART);
+
+        school = findViewById(R.id.main_school_image);
+        school.setAnimation(topAnim);
+//        school.setAnimation(bottomAnim);
 
         startBtn = findViewById(R.id.startBtn);
         startBtn.setOnClickListener(v -> {

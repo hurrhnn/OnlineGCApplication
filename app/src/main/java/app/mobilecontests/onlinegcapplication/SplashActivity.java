ca66a5015dcefa78f1c8f94a05a032d73b16bdf4
lnJoy <50934835+lnJoy@users.noreply.github.com>
2021-06-05 16:41:35 +0900

    Add splash animation

--- app/src/main/java/app/mobilecontests/onlinegcapplication/SplashActivity.java
+++ app/src/main/java/app/mobilecontests/onlinegcapplication/SplashActivity.java
@@ -1,33 +1,42 @@
 package app.mobilecontests.onlinegcapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class SplashActivity extends Activity {
+
+    private static int SPLASH_DISPLAY_LENGTH = 1600;
+
+    Animation splashAnim;
+    ImageView school;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
-        startLoading();
-    }
-    private void startLoading() {
-        Handler handler = new Handler() {
-            @Override
-            public void publish(LogRecord record) {
-            }
 
-            @Override
-            public void flush() {
-            }
+        // Animantions
+        splashAnim = AnimationUtils.loadAnimation(this, R.anim.splash_school_animation);
 
+        // Hooks
+        school = findViewById(R.id.school);
+        school.setAnimation(splashAnim);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void close() throws SecurityException {
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
             }
-        };
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }

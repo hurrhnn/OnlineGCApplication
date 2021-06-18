ba624009ad318041b3c764e98dac0893253e4c4f
hurrhnn <40728528+hurrhnn@users.noreply.github.com>
2021-06-18 22:01:18 +0900

    Added some database query.

--- app/src/main/java/app/mobilecontests/onlinegcapplication/sqlite/SQLiteHelper.java
+++ app/src/main/java/app/mobilecontests/onlinegcapplication/sqlite/SQLiteHelper.java
@@ -15,21 +15,30 @@ public class SQLiteHelper extends SQLiteOpenHelper {
         String query = "CREATE TABLE if not exists google_classroom ("
                 + "course blob,"
                 + "course_works blob);";
+        database.execSQL(query);
+
+        query = "CREATE TABLE if not exists ebs_classroom ("
+                + "account_info blob,"
+                + "lectures blob);";
 
         database.execSQL(query);
 
-//        query = "CREATE TABLE if not exists ebs_classroom ("
-//                + "_id integer primary key autoincrement,"
-//                + "course blob,"
-//                + "course_work blob);";
-//
-//        database.execSQL(query);
+        query = "CREATE TABLE if not exists school_timetable ("
+                + "date text,"
+                + "timetable blob);";
+        database.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
         String query = "DROP TABLE if exists google_classroom";
+        database.execSQL(query);
 
+        query = "DROP TABLE if exists ebs_classroom";
+        database.execSQL(query);
+        onCreate(database);
+
+        query = "DROP TABLE if exists school_timetable";
         database.execSQL(query);
         onCreate(database);
     }
